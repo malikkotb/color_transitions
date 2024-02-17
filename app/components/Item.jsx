ec047c5495846246654fcf3f8fccdba196a35894
Malik Kotb <malikkotb@icloud.com>
2024-02-17 15:10:32 +0100

    animation done

--- app/components/Item.jsx
+++ app/components/Item.jsx
@@ -1,14 +1,21 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
-export default function Item({ source, index }) {
+import Image from "next/image";
+export default function Item({ source, index, isLast }) {
   return (
-    <div className="h-[150vh] items-center justify-center flex flex-col m-0 p-0">
-      <div className="w-[45vw] max-w-[500px] h-[65vh] bg-purple-500 shadow-2xl"></div>
-      <div className="flex flex-col p-4 mt-4 gap-8 text-center">
-        <p className="text-xl font-medium">Issue #{index + 1}</p>
-        <p className="text-lg font-medium">GET HERE (Europe)</p>
-        <p className="text-sm font-medium">or visit at selected museums</p>
+    <div className={`h-[${isLast ? 100 : 125}vh] items-center justify-center flex flex-col gap-2 m-0 p-0`}>
+      <div className="mt-[-10%]">
+        {/* <div className="w-[45vw] max-w-[500px] h-[65vh] bg-purple-500 shadow-2xl"> */}
+        <Image src={source} alt={source} width={400} height={500} className="shadow-xl" />
+        {/* </div> */}
+        <div className="flex flex-col p-4 mt-4 gap-2 text-center">
+          <p className="text-xl font-medium">Issue #{index + 1}</p>
+          <p className="text-lg font-medium text-white">GET HERE (Europe)</p>
+          <p className="text-sm font-medium">
+            or visit at <span className="text-white">selected museums</span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -25,52 +25,6 @@ export default function RootLayout({ children }) {
           <p className="text-xl font-bold">info@banter.com</p>
         </header>
         {children}
-        <footer className="fixed flex justify-between items-center bottom-5 left-5 w-[95vw]">
-          <div className="w-64">
-            <p className="font-bold text-lg">
-              Backstage Banter magazine offers an exclusive peek behind the curtains of the entertainment industry, revealing untold stories, insider secrets,
-              and captivating interviews with the stars. Dive deep into the world of showbiz with us.
-            </p>
-            <p className="text-sm font-light my-2">2024 Published by Banter Records</p>
-          </div>
-          <ul className="flex flex-col gap-2">
-            <li>
-              <a className="menulink issue7" href="#issue7">
-                Issue #7
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue6" href="#issue6">
-                Issue #6
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue5" href="#issue5">
-                Issue #5
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue4" href="#issue4">
-                Issue #4
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue3" href="#issue3">
-                Issue #3
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue2" href="#issue2">
-                Issue #2
-              </a>
-            </li>
-            <li>
-              <a className="menulink issue1" href="#issue1">
-                Issue #1
-              </a>
-            </li>
-          </ul>
-        </footer>
       </body>
     </html>
   );
